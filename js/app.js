481bee50559e39c1aa4145766e330643477f10fd
jatinjack1 <37404839+jatinjack1@users.noreply.github.com>
2021-06-01 14:43:54 -0700

    testing website

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let hamberger = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+let mobilenav = document.querySelector('.mobile-nav');
+
+hamberger.addEventListener('click',function(){
+    mobilenav.classList.add('open')
+});
+
+times.addEventListener('click',function(){
+    mobilenav.classList.remove('open')
+});
+
